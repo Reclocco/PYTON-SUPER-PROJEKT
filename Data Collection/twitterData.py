cb83a72acf492e0cfd4ef828281c2c6c67405d14
Skazmen1 <56539001+Skazmen@users.noreply.github.com>
2020-05-04 14:43:21 +0200

    Update twitterData.py
    Obie funkcje działają :) pliki Csv same się tworzą więc nie dodaję do gita

--- Data Collection/twitterData.py
+++ Data Collection/twitterData.py
@@ -11,21 +11,39 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-def getTwittsForHashtag(hashtag):
 
-    csvFile = open('%s - hasztag.csv' %hashtag, 'a')
+def getTwittsForHashtag(hashtag):
+    csvFile = open('%s - hasztag.csv' % hashtag, 'a')
     csvWriter = csv.writer(csvFile)
 
     for tweet in tweepy.Cursor(api.search, hashtag, count=100,
                                lang="pl",
                                since="2017-04-03").items():
-        print(tweet.created_at, tweet.text)
+        #print(tweet.created_at, tweet.text)
         csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
 
 
-def getUserTwittsData():
-    # TODO napisać funkcję
+def getUserTwittsData(user):
+    alltweets = []
+    new_tweets = api.user_timeline(screen_name=user, count=200)
+    alltweets.extend(new_tweets)
+    oldest = alltweets[-1].id - 1
+
+    while len(new_tweets) > 0:
+        #print("downloading tweets before %s" % (oldest))
+        new_tweets = api.user_timeline(screen_name=user, count=200, max_id=oldest)
+        alltweets.extend(new_tweets)
+        oldest = alltweets[-1].id - 1
+
+        #print("...%s tweets downloaded so far" % (len(alltweets)))
+
+    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+
+    with open('%s_tweets.csv' % user, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["id", "created_at", "text"])
+        writer.writerows(outtweets)
     pass
 
-
 getTwittsForHashtag("#SYCOW")
+getUserTwittsData("polsport")
