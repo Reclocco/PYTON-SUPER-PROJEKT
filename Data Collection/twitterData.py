bb35baad937fb4e82680e1d700982b65a679e0ab
Skazmen1 <56539001+Skazmen@users.noreply.github.com>
2020-05-04 14:34:10 +0200

    Update twitterData.py

--- Data Collection/twitterData.py
+++ Data Collection/twitterData.py
@@ -1,19 +1,19 @@
 import tweepy
 import csv
 
+consumer_key = 'a71AzCqamdL5TsPFD3iuLYhWY'
+consumer_secret = '832tfVnw9394JP78sA0N3RXZQU2HxXg0aFRIMw3dUjh77LhEqS'
+access_token = '1255793475058098178-eyMC4L0jFFweQPVeRJITP1CAEjQLtI'
+access_token_secret = 'rLtyctsrzYVEpVZGPQifuuY63hxalLbir6KT04e9IwheW'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
 def getTwittsForHashtag(hashtag):
 
-    consumer_key = 'a71AzCqamdL5TsPFD3iuLYhWY'
-    consumer_secret = '832tfVnw9394JP78sA0N3RXZQU2HxXg0aFRIMw3dUjh77LhEqS'
-    access_token = '1255793475058098178-eyMC4L0jFFweQPVeRJITP1CAEjQLtI'
-    access_token_secret = 'rLtyctsrzYVEpVZGPQifuuY63hxalLbir6KT04e9IwheW'
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    api = tweepy.API(auth, wait_on_rate_limit=True)
-
-    csvFile = open('ua.csv', 'a')
+    csvFile = open('%s - hasztag.csv' %hashtag, 'a')
     csvWriter = csv.writer(csvFile)
 
     for tweet in tweepy.Cursor(api.search, hashtag, count=100,
@@ -26,5 +26,6 @@ def getTwittsForHashtag(hashtag):
 def getUserTwittsData():
     # TODO napisać funkcję
     pass
+
 
 getTwittsForHashtag("#SYCOW")
