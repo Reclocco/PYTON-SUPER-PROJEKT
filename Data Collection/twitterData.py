41d02f59a414d1a62e26653eb48af3349b9e93e4
Skazmen1 <56539001+Skazmen@users.noreply.github.com>
2020-05-04 14:18:40 +0200

    Update twitterData.py
    Działa pobieranie po hashtagu

--- Data Collection/twitterData.py
+++ Data Collection/twitterData.py
@@ -1,37 +1,30 @@
 import tweepy
-import datetime
-
+import csv
 
 def getTwittsForHashtag(hashtag):
-    # TODO założyć konto deweloperskie, zdobyć dane autoryzacyjne
-    # how to get conf data:
-    # https://towardsdatascience.com/how-to-access-twitters-api-using-tweepy-5a13a206683b
-    conf = {
-        'consumer_key': None,
-        'consumer_secret': None,
-        'access_key': None,
-        'access_secret': None
-    }
 
-    auth = tweepy.OAuthHandler(conf['consumer_key'], conf['consumer_secret'])
-    auth.set_access_token(conf['access_key'], conf['access_secret'])
-    api = tweepy.API(auth)
+    consumer_key = 'a71AzCqamdL5TsPFD3iuLYhWY'
+    consumer_secret = '832tfVnw9394JP78sA0N3RXZQU2HxXg0aFRIMw3dUjh77LhEqS'
+    access_token = '1255793475058098178-eyMC4L0jFFweQPVeRJITP1CAEjQLtI'
+    access_token_secret = 'rLtyctsrzYVEpVZGPQifuuY63hxalLbir6KT04e9IwheW'
 
-    if hashtag[0] == '#':
-        hashtag = hashtag[1:]
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-    # results = []
-    # for tweet in tweepy.Cursor(api.search, q='%23' + hashtag).items(1000):
-    #     results.append(tweet)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    filename = 'twittsFor#' + hashtag + datetime.datetime.now().strftime("-%d-%m-%y-%H:%M:%S")
+    csvFile = open('ua.csv', 'a')
+    csvWriter = csv.writer(csvFile)
 
-    with open(filename, 'w+') as f:
-        f.write(' '.join(results))
-
-    return filename
+    for tweet in tweepy.Cursor(api.search, hashtag, count=100,
+                               lang="pl",
+                               since="2017-04-03").items():
+        print(tweet.created_at, tweet.text)
+        csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
 
 
-def getMyTwittsData():
+def getUserTwittsData():
     # TODO napisać funkcję
     pass
+
+getTwittsForHashtag("#SYCOW")
