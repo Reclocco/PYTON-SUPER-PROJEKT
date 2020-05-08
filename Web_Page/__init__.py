7dc464b53595c90b5350b620433cbaac86af1736
Michał Reclik <michal-reclik@wp.pl>
2020-05-09 00:58:18 +0200

    Added templates

--- Web_Page/__init__.py
+++ Web_Page/__init__.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/home')
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/generate')
+def generate():
+    # generate tweets to pass them to website
+    tweet = "random text"
+    return render_template('generate.html', tweet=tweet)
 
 
 if __name__ == '__main__':
