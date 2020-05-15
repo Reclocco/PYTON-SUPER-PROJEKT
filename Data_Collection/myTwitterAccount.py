baf373a7113470c7b4a83441cd262a5846d1eee8
Skazmen1 <56539001+Skazmen@users.noreply.github.com>
2020-05-15 17:29:22 +0200

    Update myTwitterAccount.py

--- Data_Collection/myTwitterAccount.py
+++ Data_Collection/myTwitterAccount.py
@@ -17,19 +17,10 @@ def deleteLastTweet():
     if len(lastTweets) > 0:
         lastId = lastTweets[0]._json['id']
         api.destroy_status(lastId)
-        
-        
-def deleteTweet(number):
-    myAccountName = api.me()._json['screen_name']
-    tweets = api.user_timeline(screen_name=myAccountName, count=1)
-    if len(tweets) > 0:
-        id = tweets[number]._json['id']
-        api.destroy_status(id)
-
+       
 
 if __name__ == '__main__':
     # postTweet('Yey, I will be a bot and this is test tweet')
-    #deleteTweet(0) # 0, -1, -2  w tę stronę
     # getMyTweetsData(100)
     # deleteLastTweet()
     pass
