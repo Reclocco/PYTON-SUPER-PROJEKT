a8045a5972190f6437300de193db4fba92e68798
Skazmen1 <56539001+Skazmen@users.noreply.github.com>
2020-05-29 13:53:53 +0200

    description

--- Statistics/sentiment.py
+++ Statistics/sentiment.py
@@ -63,3 +63,6 @@ def getData():
 
 if __name__ == '__main__':
     getData()
+#Sentiment Analysis is the process of ‘computationally’ 
+#determining whether a piece of writing is positive, negative 
+#or neutral. It’s also known as opinion mining, deriving the opinion or attitude of a speaker.
