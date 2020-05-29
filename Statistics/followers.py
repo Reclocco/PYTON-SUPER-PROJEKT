14b73fcb1d6f79ac45bd4668354096cfcea2bbd7
Skazmen1 <56539001+Skazmen@users.noreply.github.com>
2020-05-29 14:22:01 +0200

    Create followers.py

---
+++ Statistics/followers.py
@@ -0,0 +1,14 @@
+from Data_Collection.twitterData import api
+from config import *
+
+
+def get_followers(person):
+    count=0
+
+    influencer=api.get_user( screen_name=person)
+    number_of_followers=influencer.followers_count
+    print("number of followers count : ", number_of_followers)
+    return count
+
+user = api.me()._json['screen_name']
+get_followers(user)
